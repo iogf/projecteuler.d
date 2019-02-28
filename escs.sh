714c83c813c8454279c35011256992bf3b65bc38
Iury O. G. Figueiredo <last.src@gmail.com>
2019-02-28 17:13:35 -0300

    Adding problelm1.

---
+++ escs.sh
@@ -0,0 +1,10 @@
+cd ~/projects/projecteuler.golang-code
+git status
+git add *
+git commit -a
+git push -u origin master
+
+
+# test problem-5
+cd ~/projects/projecteuler.golang-code
+go run problem-5.go
